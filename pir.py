eea92ba5f51be5cc2efe79556281ffc2629f7358
ChrisDeBank <cdb3g14@soton.ac.uk>
2018-07-10 19:39:24 +0100

    PIR testbed

--- pir.py
+++ pir.py
@@ -2,3 +2,11 @@
 # PIR Test Bed
 #-------------------------------------------------------------------------------
 
+#PIR output connected to GPIO2 (Physical Pin 3)
+
+import gpiozero import MotionSensor
+
+pir = MotionSensor(2)
+
+pir.wait_for_motion()
+print("Motion Detected!!)
