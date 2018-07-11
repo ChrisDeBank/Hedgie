2bd37af4c40550bde8d5e29a87c10f7ca59e5c01
ChrisDeBank <debank.c@gmail.com>
2018-07-11 18:04:02 +0100

    PIR test work

--- pir.py
+++ pir.py
@@ -4,9 +4,8 @@
 
 #PIR output connected to GPIO2 (Physical Pin 3)
 
-import gpiozero import MotionSensor
+from gpiozero import MotionSensor
 
-pir = MotionSensor(2)
-
+pir = MotionSensor(4)
 pir.wait_for_motion()
-print("Motion Detected!!)
+print("Motion Detected!!")
